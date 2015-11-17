787561ae93d655201a7398c1c03ea308195e9c45
younessellaoui <yousell92@hotmail.fr>
2015-11-17 14:15:24 +0100

    step 2

--- lib/app.jsx
+++ lib/app.jsx
@@ -9,9 +9,9 @@ var Box = React.createClass({
    */
   'render': function onRender () {
     return (
-      <button>X</button>
+      <button>{this.props.value}</button>
     );
   }
 });
 
-React.render(<Box/>, document.body);
+React.render(<Box value='X'/>, document.body);
