1bf77268237f3ae06a7b6b7aefd2ed6a94628e0d
younessellaoui <yousell92@hotmail.fr>
2015-11-17 15:51:10 +0100

    step 4 and 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -3,17 +3,39 @@
 var React = require('react');
 
 var boxStyle = {
-            'height': '100px',
-            'width': '100px',
-            'background-color':'green'
+            'height': '150px',
+            'width': '150px',
+            'background-color':'yellow'
 };
 
+var time = 1000;
 var Box = React.createClass({
+       'getInitialState': function onGestInitialState(){
+            return{
+                'value' : this.props.initialValue
+                  };
+        },
+
+       componentWillMount: function(){
+          var old_this = this;
+          this.timer = setInterval(function(){
+
+          var oldValue = old_this.state.value;
+          var newValue = oldValue === 'X' ? 'O': 'X';
+          old_this.setState({
+          value: newValue
+          });
+         }, time);
+       },
+
+       componentWillUnmount: function(){
+   clearInterval(this.timer);},
+
   'render': function onRender () {
-    return (
-      <button style = {boxStyle}>{this.props.value}</button>
+      return (
+      <button style = {boxStyle}>{this.state.value}</button>
     );
   }
 });
 
-React.render(<Box value='X'/>, document.body);
+React.render(<Box initialValue='X'/>, document.body);
